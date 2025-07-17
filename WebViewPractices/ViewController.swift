7d02c065238828a8acb27d6a41d01be4e88ce0ae
haisombo <haisombo20@gmail.com>
2025-07-17 09:25:20 +0700

    Initial Commit

---
+++ WebViewPractices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewPractices
+//
+//  Created by Sombo Mobile R&D on 17/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WebViewPracticesTests/WebViewPracticesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewPracticesTests.swift
+//  WebViewPracticesTests
+//
+//  Created by Sombo Mobile R&D on 17/7/25.
+//
+
+import Testing
+@testable import WebViewPractices
+
+struct WebViewPracticesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
